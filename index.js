a091b9f95569593c6fef843efa737cfc6db10a7c
Sophia S. <sophia0791@icloud.com>
2023-12-14 16:34:25 +0100

    add answer toggle

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 // phew… not a lot going on here. Please add some code!
+
+// bookmark toggle button
 const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
 
 bookmarkButton.addEventListener("click", () => {
     bookmarkButton.classList.toggle("bookmark--active");
+})
+
+// show answer button
+
+const showAnswerButton = document.querySelector('[data-js="show-answer"]');
+const answer = document.querySelector('[data-js="cardAnswer"]');
+
+showAnswerButton.addEventListener("click", () => {
+    answer.classList.toggle("card__answer--active");
 })
